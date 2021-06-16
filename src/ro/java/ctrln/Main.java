dab34d8830be09e3265aa41eba329439b4f6d731
Catalin Murgoci <catalin.catalin729@yahoo.com>
2021-06-16 22:27:35 +0300

    Modificare Main.java

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,9 +1,9 @@
 package ro.java.ctrln;
 
 public class Main {
-    //JAR - Java Archive
     //JAVA_HOME
     public static void main(String[] args) {
+
 	    System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
     }
 }
