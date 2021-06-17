e2915a95b6ed4c075297f4b6252daf1e1939a9c0
Catalin Murgoci <catalin.catalin729@yahoo.com>
2021-06-17 03:44:37 +0300

    Modificare Main.java - stergere comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,7 +1,6 @@
 package ro.java.ctrln;
 
 public class Main {
-    //JAVA_HOME
     public static void main(String[] args) {
 
 	    System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
